b7733f92a7c152688fdb928b5b3053b45dcd2352
yonny <ycnovac@unal.edu.co>
2024-02-18 11:06:46 -0500

    upgrading uts

--- src/app/components/list-recipie/list-recipie.component.spec.ts
+++ src/app/components/list-recipie/list-recipie.component.spec.ts
@@ -5,17 +5,15 @@ import { ActivatedRoute, Router, RouterLink, convertToParamMap } from '@angular/
 import { mockRecipe } from '../../../mocks/mock-recipe';
 import { RecipiesService } from '../../services/recipies.service';
 import { MockRecipiesService } from '../../../mocks/mock-recipies.service';
-import { HarnessLoader } from '@angular/cdk/testing';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import { MatIconHarness } from '@angular/material/icon/testing';
 
-describe('ListRecipieComponent', () => {
+
+fdescribe('ListRecipieComponent', () => {
   let component: ListRecipieComponent;
   let fixture: ComponentFixture<ListRecipieComponent>;
   let recipeService:RecipiesService;
-  let loader: HarnessLoader;
+
   let router: Router;
   let navigateSpy: jasmine.Spy;
 
@@ -49,7 +47,6 @@ describe('ListRecipieComponent', () => {
     recipeService = TestBed.inject(RecipiesService);
     component = fixture.componentInstance;
     recipeService.getRecipies();
-    loader = TestbedHarnessEnvironment.loader(fixture);
     router = TestBed.inject(Router);
     navigateSpy = spyOn(router, 'navigate').and.stub();
     fixture.detectChanges();
@@ -62,10 +59,8 @@ describe('ListRecipieComponent', () => {
   it('should call deleteRecipe method when delete button is clicked', async () => {
     spyOn(component, 'deleteRecipe'); 
 
-    const deleteButton = await loader.getHarness(MatIconHarness.with({ name: 'delete' })); 
-    const deleteButtonElement = await deleteButton.host(); 
-    await deleteButtonElement.click(); 
-
+    let deleteBtn = fixture.nativeElement.querySelector('div.recipie-btns > mat-icon:nth-child(2)') as HTMLButtonElement; 
+    deleteBtn.click();
     await fixture.detectChanges();
 
     expect(component.deleteRecipe).toHaveBeenCalledWith(mockRecipe.recipes[0].id);
